1fb77e22e75ef7475a44fb8e37c48af48ed8f2ad
YUPANZHAO <2332998427@qq.com>
2023-03-15 08:27:59 +0000

    Add MenuItem component

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p class="MenuName">{{ MenuName }}</p>
+    <p v-for="item in Items" :key="item" class="Item">
+      {{ item.name }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'MenuName',
+        'Items',
+    ],
+}
+</script>
+
+<style>
+.MenuName {
+  font-family: Georgia, serif;
+  font-weight: bold;
+  font-size: 22px;
+  margin: 0;
+  margin-bottom: 15px;
+}
+.Item {
+  font-family: Georgia, serif;
+  font-size: 16px;
+  margin: 0;
+  margin-bottom: 5px;
+  max-width: 260px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="root">
-    <NameCard :class="classNameCard "
+    <NameCard :class="classNameCard"
       AvatarUrl="https://img1.imgtp.com/2023/03/15/Yu7q4P8x.jpg"
       AvatarSize="100px"
       Name="YUPAN ZHAO"
       Description="Huizhou University"
       :IconGroup="iconGroup" />
+    <div :class="classMenu">
+      <MenuItem :class="classMenuItem"
+        MenuName="最近文章1"
+        :Items="menuItems_One" />
+      <MenuItem :class="classMenuItem"
+        MenuName="最近文章2"
+        :Items="menuItems_Two" />
+      <MenuItem :class="classMenuItem"
+        MenuName="最近文章3"
+        :Items="menuItems_Three" />
+      <MenuItem :class="classMenuItem"
+        MenuName="最近文章4"
+        :Items="menuItems_Four" />
+    </div>
   </div>
 </template>
 
 <script>
 import NameCard from '../components/NameCard.vue'
+import MenuItem from '../components/MenuItem.vue'
 export default {
   components: {
-    NameCard
+    NameCard,
+    MenuItem,
   },
   data() {
     return {
@@ -35,11 +51,76 @@ export default {
           hint: "个人博客",
         },
       ],
+      menuItems_One: [
+        {
+          name: "基于 RTMP 协议实现的家庭监控系统",
+        },
+        {
+          name: "C++ 学习笔记",
+        },
+        {
+          name: "三国杀华容道",
+        },
+        {
+          name: "📝记账APP",
+        },
+      ],
+      menuItems_Two: [
+        {
+          name: "基于 RTMP 协议实现的家庭监控系统",
+        },
+        {
+          name: "C++ 学习笔记",
+        },
+        {
+          name: "三国杀华容道",
+        },
+        {
+          name: "📝记账APP",
+        },
+      ],
+      menuItems_Three: [
+        {
+          name: "基于 RTMP 协议实现的家庭监控系统",
+        },
+        {
+          name: "C++ 学习笔记",
+        },
+        {
+          name: "三国杀华容道",
+        },
+        {
+          name: "📝记账APP",
+        },
+      ],
+      menuItems_Four: [
+        {
+          name: "基于 RTMP 协议实现的家庭监控系统",
+        },
+        {
+          name: "C++ 学习笔记",
+        },
+        {
+          name: "三国杀华容道",
+        },
+        {
+          name: "📝记账APP",
+        },
+      ],
     }
   },
   computed: {
+    isPhone() {
+      return this.screenHeight / this.screenWidth > 0.6 || this.screenWidth <= 850;
+    },
     classNameCard() {
-      return this.screenHeight > this.screenWidth ? 'NameCard-Phone' : 'NameCard';
+      return this.isPhone ? 'NameCard-Phone' : 'NameCard';
+    },
+    classMenu() {
+      return this.isPhone ? 'Menu-Phone' : 'Menu';
+    },
+    classMenuItem() {
+      return this.isPhone ? 'MenuItem-Phone' : 'MenuItem';
     },
   },
   methods: {
@@ -71,18 +152,46 @@ export default {
 .root {
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
 }
 .NameCard-Phone {
   width: fit-content;
   margin: 0 auto;
-  position: relative;
-  top: 13%;
+  position: absolute;
+  top: 10%;
 }
 .NameCard {
   width: fit-content;
+  height: fit-content;
   position: relative;
   top: 50%;
   transform: translateY(-50%);
-  left: 25%
+}
+.Menu-Phone {
+  width: fit-content;
+  position: absolute;
+  margin: 0 auto;
+  top: calc(10% + 240px);
+  margin-bottom: 200px;
+}
+.Menu {
+  width: fit-content;
+  height: fit-content;
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-rows: 1fr 1fr;
+  grid-row-gap: 30px;
+  grid-column-gap: 30px;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  margin-left: 150px;
+}
+.MenuItem-Phone {
+  margin-bottom: 30px;
+}
+.MenuItem {
+  width: fit-content;
 }
 </style>
