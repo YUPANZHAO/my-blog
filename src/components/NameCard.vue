445c9256c152b9d80ea8da149ebd9c82bce85d3c
YUPANZHAO <2332998427@qq.com>
2023-03-15 05:42:17 +0000

    Add NameCard component

---
+++ src/components/NameCard.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <img :src="AvatarUrl"
+      class="avatar" :style="{ width:AvatarSize, height:AvatarSize }">
+    <p class="Name"> {{ Name }} </p>
+    <p class="Description"> {{ Description }} </p>
+    <ul class="IconGroup" >
+      <li v-for="icon in IconGroup" :key="icon">
+        <a :href="icon.link" target="_blank" :hint="icon.hint" class="tooltip">
+          <img class="Icon" :src="icon.url">
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'AvatarUrl',
+    'AvatarSize',
+    'Name',
+    'Description',
+    'IconGroup',
+  ],
+}
+</script>
+
+<style scoped>
+.avatar {
+  border-radius: 50%;
+  display: block;
+  margin: 0 auto;
+  box-shadow: 0px 0px 10px #888888;
+}
+.Name {
+  font-family: Georgia, serif; 
+  font-size: 22px;
+  width: fit-content;
+  margin: 0 auto;
+  margin-top: 10px;
+}
+.Description {
+  font-family: Georgia, serif;
+  color: gray;
+  font-size: 14px;
+  width: fit-content;
+  margin: 0 auto; 
+  margin-top: 10px;
+}
+.IconGroup {
+  display: flex;
+  justify-content: center;
+  List-style-type: none;
+  margin: 0;
+  padding: 0;
+  margin-top: 10px;
+  height: 20px;
+}
+.Icon {
+  width: 20px;
+  height: 20px;
+  padding-inline: 5px;
+}
+.tooltip {
+  position: relative;
+}
+.tooltip:hover::before {
+  word-break: keep-all;
+  white-space: nowrap;
+  content: attr(hint);
+  position: absolute;
+  padding: 2px 6px;
+  display: block;
+  color: #333;
+  border: 1px solid #333;
+  border-radius: 5px;
+  font-size: 12px;
+  line-height: 20px;
+  top: 25px;
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: 0px 0px 10px #eaeaea;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,88 @@
 <template>
-
+  <div class="root">
+    <NameCard :class="classNameCard "
+      AvatarUrl="https://img1.imgtp.com/2023/03/15/Yu7q4P8x.jpg"
+      AvatarSize="100px"
+      Name="YUPAN ZHAO"
+      Description="Huizhou University"
+      :IconGroup="iconGroup" />
+  </div>
 </template>
 
 <script>
+import NameCard from '../components/NameCard.vue'
 export default {
-  
+  components: {
+    NameCard
+  },
+  data() {
+    return {
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+      iconGroup: [
+        { 
+          url: "https://img1.imgtp.com/2023/03/15/6OB2Q7Go.png",
+          link: "https://github.com/YUPANZHAO",
+          hint: "GitHub",
+        },
+        { 
+          url: "https://img1.imgtp.com/2023/03/15/CCetn0ZT.png",
+          hint: "yupanzhao2022@163.com",
+        },
+        { 
+          url: "https://img1.imgtp.com/2023/03/15/PK67VI4A.png", 
+          link: "http://47.115.209.240:8080",
+          hint: "个人博客",
+        },
+      ],
+    }
+  },
+  computed: {
+    classNameCard() {
+      return this.screenHeight > this.screenWidth ? 'NameCard-Phone' : 'NameCard';
+    },
+  },
+  methods: {
+    windowSizeChange() {
+      this.screenWidth = window.innerWidth;
+      this.screenHeight = window.innerHeight;
+    },
+  },
+  mounted() {
+    this.windowSizeChange();
+    window.onresize = () => {
+      if(!this.timer) {
+        this.timer = true;
+        let that = this;
+        setTimeout(function() {
+          that.windowSizeChange();
+          that.timer = false;
+        }, 400);
+      }
+    };
+  },
+  destroyed() {
+    window.onresize = null;
+  },
 }
 </script>
 
 <style>
-
+.root {
+  width: 100%;
+  height: 100%;
+}
+.NameCard-Phone {
+  width: fit-content;
+  margin: 0 auto;
+  position: relative;
+  top: 13%;
+}
+.NameCard {
+  width: fit-content;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 25%
+}
 </style>
